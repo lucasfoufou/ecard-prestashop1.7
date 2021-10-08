c07c0b2784b290d02604fe3d6361c3137134e4b6
Lucas Fougeras <lucasfougeras@DIGITALCACTUS>
2021-10-08 15:45:45 +0200

    v2.2.2

--- README.md
+++ README.md
@@ -2,8 +2,13 @@
 Repo pour prestashop 1.7
 
 ## Changelog
+### 2.2.2
+- Only load Js and Css when the controller is "AdminPledg"
+- Add a security to prevent the plugin from reverting a notification during the validation (in a case where the validation happens after the notification)
+  
 ### 2.2.1
 - Fix Notice Undefined property: Cart::getCurrency
+  
 ### 2.1.5 to 2.2.0
 - Change all static calls to `Logger` to `PrestaShopLogger` to help IDEs
 - PledgParams: concatenate `Address` and `Address complement` to send to Pledg API
--- controllers/front/validation.php
+++ controllers/front/validation.php
@@ -25,53 +25,59 @@ class PledgValidationModuleFrontController extends ModuleFrontController
 
         $reference = null;
         $reference = $_POST['reference'] ?? $_POST['transaction'];
-        PrestaShopLogger::addLog(sprintf($this->module->l('Pledg Payment Validation - Reference payment : %s'),$reference));
+        PrestaShopLogger::addLog(sprintf($this->module->l('Pledg Payment Validation - Reference payment : %s'), $reference));
 
         if (empty($reference)) {
-            PrestaShopLogger::addLog($this->module->l('Pledg Payment Validation - Reference payment is null'),2);
+            PrestaShopLogger::addLog($this->module->l('Pledg Payment Validation - Reference payment is null'), 2);
         }
 
         $cartId = intval(str_replace(Pledg::PLEDG_REFERENCE_PREFIXE, '', $reference));
         if (!is_int($cartId)) {
             PrestaShopLogger::addLog(
-                sprintf($this->module->l('Pledg Payment Validation - Reference ID doesn\'t seems to be a associated to a Cart : %s'),
-                    $cartId),
-                2);
+                sprintf(
+                    $this->module->l('Pledg Payment Validation - Reference ID doesn\'t seems to be a associated to a Cart : %s'),
+                    $cartId
+                ),
+                2
+            );
             Tools::redirect('index.php?controller=order&step=1');
             exit;
         }
-        
+
         $cart = new Cart($cartId);
         $order = new Order(Order::getIdByCartId((int)$cartId));
         if (!Validate::isLoadedObject($cart) && !Validate::isLoadedObject($order)) {
-            PrestaShopLogger::addLog(sprintf($this->module->l('Pledg Payment Validation - Cart doesn\t exist : '),$cartId),2);
+            PrestaShopLogger::addLog(sprintf($this->module->l('Pledg Payment Validation - Cart doesn\t exist : '), $cartId), 2);
             Tools::redirect('index.php?controller=order&step=1');
         }
-		$currency = new Currency((int) $cart->id_currency);
-        $customer = New Customer($cart->id_customer);
+        $currency = new Currency((int) $cart->id_currency);
+        $customer = new Customer($cart->id_customer);
         $priceConverted = $this->context->currentLocale->formatPrice($cart->getOrderTotal(), $currency->iso_code);
-        if(!Validate::isLoadedObject($order)){
-            $this->module->validateOrder(
-                (int)($cartId),
-                Configuration::get('PLEDG_STATE_WAITING_NOTIFICATION'),
-                $priceConverted,
-                $this->module->name,
-                null,
-                null,
-                null,
-                false,
-                $customer->secure_key
-            );
-        }
-        else{
-            PrestaShopLogger::addLog(sprintf($this->module->l('Pledg Payment Validation - Reference ID has already been validated by notification : %s'),$cartId));
+        if (!Validate::isLoadedObject($order)) {
+            if ($order->current_state !== Configuration::get("PS_OS_OUTOFSTOCK_PAID") && $order->current_state !== Configuration::get("PS_OS_PAYMENT")) {
+                $this->module->validateOrder(
+                    (int)($cartId),
+                    Configuration::get('PLEDG_STATE_WAITING_NOTIFICATION'),
+                    $priceConverted,
+                    $this->module->name,
+                    null,
+                    null,
+                    null,
+                    false,
+                    $customer->secure_key
+                );
+            } else {
+                Logger::addLog(sprintf($this->module->l('Pledg Payment Validation - Notification already processed : %s'), $cartId));
+            }
+        } else {
+            PrestaShopLogger::addLog(sprintf($this->module->l('Pledg Payment Validation - Reference ID has already been validated by notification : %s'), $cartId));
         }
         Tools::redirect(
-            'index.php?controller=order-confirmation&id_cart='.
-            (int)$cart->id.
-            '&id_module='. (int)$this->module->id.
-            '&id_order='.$order->id.
-            '&key='.$customer->secure_key
+            'index.php?controller=order-confirmation&id_cart=' .
+                (int)$cart->id .
+                '&id_module=' . (int)$this->module->id .
+                '&id_order=' . $order->id .
+                '&key=' . $customer->secure_key
         );
     }
 }
